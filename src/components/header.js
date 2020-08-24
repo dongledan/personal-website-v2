5455e2c5d010c454dfc3cc4475ed8c6c4af16f13
Danny Li <dongledan@yahoo.com>
2020-08-24 16:01:25 -0400

    header layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,33 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import Typed from 'typed.js';
+import React, { Component } from 'react'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import { typewriterContentOptions, typewriterTitleOptions } from '../content/headerContent';
+import Image from './image'
+export default class Header extends Component {
+  constructor(props) {
+    super(props)
+  }
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+  componentDidMount() {
+    this.typed = new Typed(this.typewriter, typewriterContentOptions);
+    this.typedTitle = new Typed(this.title, typewriterTitleOptions);
+  }
+
+  componentWillUnmount() {
+    this.typed.destroy();
+    this.typedTittle.destory();
+  }
+  render() {
+    
+    return (
+      <div className="header-container">
+        <Image />
+        <span className="header-title" ref={title => {this.title = title}}></span>
+        <span className="header-content" ref={typewriter => {this.typewriter = typewriter; }}></span>
+      </div>
+    )
+  }
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
