6729e3e8f7ef40ade8854eaede424f01684ebaf5
Danny Li <dongledan@yahoo.com>
2020-08-27 14:22:26 -0400

    outline layout

--- src/pages/outlines.js
+++ src/pages/outlines.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Outlines from '../components/Outlines';
 
 const outlines = () => (
     <Layout>
         <SEO title="Outlines" />
+        <Outlines />
     </Layout>
 );
 
