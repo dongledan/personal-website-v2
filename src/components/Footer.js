2b41f1df5a364f382222447bb5a48e0684a8982e
Danny Li <dongledan@yahoo.com>
2020-08-29 18:10:19 -0400

    added social icons to footer

---
+++ src/components/Footer.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FaGithub,  FaLinkedinIn, FaEnvelope, FaApple, FaGooglePlay} from "react-icons/fa";
+
+import { scrollToTop } from "../utils";
+
+export default function Footer() {
+ return (
+  <footer className="footer-container">
+
+   <div className=" copyright-container">
+   <div className="footer-copyright">
+   © {new Date().getFullYear()} Danny Li ✌️
+   </div>
+   <div className="divider">{"\xa0"}</div>
+   <div className="footer-social-container">
+    <a className="footer-social mail"  href="mailto:dli0906@gmail.com">
+     <FaEnvelope />
+    </a>
+    <a className="footer-social linkedin" rel="noopener noreferrer"
+        target="_blank"
+        href="https://www.linkedin.com/in/d-li">
+     <FaLinkedinIn />
+    </a>
+    <a className="footer-social github"  rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/dongledan">
+     <FaGithub />
+    </a>
+    <a className="footer-social google" rel="noopener noreferrer"
+        target="_blank" href="https://play.google.com/store/apps/developer?id=dongledan&hl=en_US">
+     <FaGooglePlay />
+    </a>
+    <a className="footer-social apple" rel="noopener noreferrer"
+        target="_blank" href="https://apps.apple.com/us/developer/danny-li/id1509374209">
+     <FaApple />
+    </a>
+  
+    </div>
+   </div>
+    <div className="back-to-top-container">
+        <a
+            className="back-to-top"
+            onClick={() => scrollToTop()}
+        >
+            <div className="arrow">^</div>
+            Back to top
+
+        </a>
+    </div>
+</footer>
+ )
+}
