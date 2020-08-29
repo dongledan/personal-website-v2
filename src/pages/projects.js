3f0e9525a47bf1c87c9ef0c53360aee6e8f0f10a
Danny Li <dongledan@yahoo.com>
2020-08-29 19:11:32 -0400

    added styles to project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "../scss/index.scss";
+
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Projects from "../components/Projects";
