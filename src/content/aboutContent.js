9e21ea981aa8175e79daf3df971b8bafaa0b19d9
Danny Li <dongledan@yahoo.com>
2020-09-08 23:39:28 -0400

    color shade changes & content tweaks

--- src/content/aboutContent.js
+++ src/content/aboutContent.js
@@ -6,11 +6,11 @@ export const aboutMe = [
     {
         input: "Danny.skills",
         output:
-            '[ "Javascript", "Node.js", "Express", "React", "Redux", "HTML5", "CSS3", "SASS", "webpack", "git" ]',
+            '[ "Javascript", "Node.js", "Express", "React", "Redux", "HTML5", "CSS3", "SASS", "webpack", "git", "gimp" ]',
     },
     {
         input: "Danny.education",
-        output: `[ "Software Engineering Immersive - Fullstack Academy of Code", "B.A. | Accounting & Economics - CUNY Queens College" ]`,
+        output: `[ "Software Engineering Immersive - Fullstack Academy of Code", "B.A. in Accounting & Economics - CUNY Queens College" ]`,
     },
 
     {
